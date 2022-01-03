b23b18681e966adcdb45c7d0e4f009585367158a
monalisaputri <monalisa.putri@mhs.itenas.ac.com>
2022-01-03 23:02:24 +0700

    Menambahkan fungsionalitas add dan sub

---
+++ src/com/oop/git/Aritmatika.java
@@ -0,0 +1,17 @@
+package com.oop.git;
+
+public class Aritmatika {
+	//Developer 1 
+	public double add(int a, int b) {
+		return a+b;
+	}
+	
+	//Developer 1
+	public double sub (int a, int b) {
+		return a - b;
+	}
+	
+	//Developer 2(mult)
+	
+	//Developer 3(div)
+}
